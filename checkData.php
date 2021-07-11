6d9b474a62d373420e0ee1294a54a0277e9fb06a
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2021-07-11 22:31:14 +0100

    Update checkData.php

--- checkData.php
+++ checkData.php
@@ -1,5 +1,39 @@
 <?php
-    function checkData($data, $illegalCode, $replacement): string
+    function checkData($mode, $data)
+    {
+        global $conn;
+
+        $data = mysqli_real_escape_string($conn, $data);
+        $data = dataChecker($data, '<?', '&lt;?');
+        $data = dataChecker($data, '<script src=', '&lt;script src=');
+        $data = dataChecker($data, '<script src =', '&lt;script src =');
+        $data = dataChecker($data, '<link', '&lt;link');
+        if ($mode == 'DEFAULT') {
+            $data = htmlspecialchars($data);
+            $data = dataChecker($data, '<script', '&lt;script');
+            $data = dataChecker($data, '<css', '&ltstyle');
+            $data = dataChecker($data, 'css =', 'blocked =');
+            $data = dataChecker($data, 'css=', 'blocked=');
+        } elseif ($mode == 'HTML') {
+            $data = dataChecker($data, '<script', '&lt;script');
+            $data = dataChecker($data, '<css', '&ltstyle');
+            $data = dataChecker($data, 'css =', 'blocked =');
+            $data = dataChecker($data, 'css=', 'blocked=');
+        } elseif ($mode == 'TAGCSS') {
+            $data = dataChecker($data, '<script', '&lt;script');
+            $data = dataChecker($data, '<css', '&ltstyle');
+        } elseif ($mode == 'CSS') {
+            $data = dataChecker($data, '<script', '&lt;script');
+        } elseif ($mode == 'JS') {
+            $data = dataChecker($data, '<css', '&ltstyle');
+            $data = dataChecker($data, 'css =', 'blocked =');
+            $data = dataChecker($data, 'css=', 'blocked=');
+        }
+
+        return $data;
+    }
+
+    function dataChecker($data, $illegalCode, $replacement): string
     {
         if (strpos($data, $illegalCode) !== false) {
             $data = str_ireplace($data, $illegalCode, $replacement);
