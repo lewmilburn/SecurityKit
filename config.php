85adcdc31e254817fb5ec57163719a5618f7518e
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2021-09-22 11:14:04 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+// WARNING - These should be set to TRUE at all times!
+// You should only change these to false in a DEVELOPMENT ENVIRONMENT when DEBUGGING.
+const SK_BLOCK_EXTERNALS = true;
+const SK_BLOCK_INJECTION = true;
