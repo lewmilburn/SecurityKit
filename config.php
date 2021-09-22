a0268218bfc168501ca9f062158abe54e73d4681
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2021-09-22 11:16:56 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
+// ---- BLOCKING SETTINGS ----
 // WARNING - These should be set to TRUE at all times!
-// You should only change these to false in a DEVELOPMENT ENVIRONMENT when DEBUGGING.
+// They're only here incase you need to disable them to debug in a DEVELOPMENT ENVIRONMENT ONLY.
 const SK_BLOCK_EXTERNALS = true;
 const SK_BLOCK_INJECTION = true;
+
+// ---- CHECKSUM HASH ----
+// Changing this will make all existing checksums invalid.
+const SK_CHECKSUM_HASH = 'sha512';
